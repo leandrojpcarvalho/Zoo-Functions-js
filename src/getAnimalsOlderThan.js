e96a5bde2f8644ad43681fc5d9c146736d106888
Leandro <ljpcarvalho@gmail.com>
2023-05-11 10:00:53 -0300

    quesito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const { species } = data;
+  const { residents } = species.find((specie) => specie.name === animal);
+
+  return residents.every((pet) => pet.age > age);
 };
 
 module.exports = getAnimalsOlderThan;
