0c6991fd4179436bb5a3b322fb06d839c8ee44f7
Leandro <ljpcarvalho@gmail.com>
2023-05-11 16:15:48 -0300

    quesito 6 e 7?

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,23 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('se o parametro passado for undefined retorna undefined', () => {
+    expect(handlerElephants()).toBe();
+  });
+  it('se o parametro passado for diferente de uma string retorna "Parâmetro inválido, é necessário uma string"', () => {
+    expect(handlerElephants(['elephants'])).toBe('Parâmetro inválido, é necessário uma string');
+    expect(handlerElephants({ species: 'elephants' })).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('verifica se passar um parametro invalido retorna null', () => {
+    expect(handlerElephants('elephants')).toBe(null);
+  });
+  it('verifica se ao passar o parametro count retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('verifica se ao passar o parametro names retorna um array com o nome dos elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('verifica se ao passar o parametro avaregeAge retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+});
