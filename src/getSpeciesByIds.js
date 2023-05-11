1f50be8e6a5cfb42fca97c7d0bba0289b84f7fed
Leandro <ljpcarvalho@gmail.com>
2023-05-11 09:38:15 -0300

    quesito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  const arrOfIds = [...ids];
+  const animals = [];
+  const { species } = data;
+  arrOfIds.forEach((id) => {
+    animals.push(species.filter((specie) => specie.id === id));
+  });
+  return animals.flat();
 };
 
 module.exports = getSpeciesByIds;
